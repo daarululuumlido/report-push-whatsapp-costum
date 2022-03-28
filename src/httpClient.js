47ac0f4198b7af7e3fbb08c34085bf6c27e1ed78
Hasan Basri <hasanbasri1493@gmail.com>
2022-03-28 14:56:35 +0700

    fix parse numbers

--- src/httpClient.js
+++ src/httpClient.js
@@ -1,16 +1,16 @@
 const axios = require("axios");
 const context = require('@actions/github');
-const {forEach} = require("../dist");
+let commits;
+
+function myFunction(commit) {
+    commits += `${commit.id.substring(0, 8)} - ${commit.message}\n`;
+}
 
 function report({url, numbers}) {
     let github = context.context.payload;
     console.log(JSON.stringify(github));
     const commitHead = github.head_commid.id.substring(0,8);
-    let commits = "";
-    forEach(github.commits, (commit) => {
-        commits += `${commit.id.substring(0,8)} - ${commit.message}\n`;
-    });
-
+    github.commits.forEach(myFunction);
     const bodyData = `Triggered via push by *${github.actor}* action ${github.repository.default_branch} ${commitHead}
 *GitHub Actions**Workflow Deploy to Staging job deployment triggered by push is SUCCESS* for _release/production_
 ${github.head_commit.url}
@@ -45,14 +45,13 @@ _SUCCESS_`;
     for (let i = 0; i < numbersSend.length; i++) {
         const data = JSON.stringify({
             "chatId": `${numbersSend[i]}`,
-            "body": "asdasd"
+            "body": bodyData
         });
         const config = {
             method: 'post',
             url: url,
             headers: {
-                'Content-Type': 'application/json',
-                'Cookie': 'connect.sid=s%3ARPpaOmzVCl4j8kY9dGRutM-o4-_aRShw.%2FhyCSqB3aC9vr3o3e3%2FlYrPCO5LA9qp0ISLXCRZDea0'
+                'Content-Type': 'application/json'
             },
             data: data
         };
