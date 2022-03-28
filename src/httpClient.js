51f8bc43c11676e3c3b5f6e61895c21bfbf8cbb7
Hasan Basri <hasanbasri1493@gmail.com>
2022-03-28 13:52:40 +0700

    fix param update version

--- src/httpClient.js
+++ src/httpClient.js
@@ -1,21 +1,24 @@
 const axios = require("axios");
 
-
-function report(url, numbers, dataGithub) {
+function report({url, numbers, dataGithub, actions}) {
+    console.log(url)
+    console.log(numbers)
     console.log(dataGithub)
-    const body = `Triggered via push by *fdg dfg * action release/production $df gdfg 
-*GitHub Actions*
-
-*Workflow Deploy to Staging job deployment triggered by push is SUCCESS* for _release/production_https://github.com/fjogeleit/http-request-action
-
-fdg sdfg dsfg- 1 commits
+    console.log(actions)
+    const data = JSON.stringify({
+        "chatId":numbers,
+        "body": `Triggered via push by *andryluthfi* action release/production 5644a10b
+*GitHub Actions**Workflow Deploy to Staging job deployment triggered by push is SUCCESS* for _release/production_
+https://github.com/fjogeleit/http-request-action
+5644a10b - 1 commits
 Commits
-ds fsdf sdf sd - temp
-
-Job Steps\n✅ bc49a7674c9c4914b05d01831d601745\n✅ extract_branch\n✅ deploy_staging
+5644a10b - temp
+Job Steps
+✅ bc49a7674c9c4914b05d01831d601745
+✅ extract_branch
+✅ deploy_staging
 
 *Workflow*
-
 _Deploy to Staging_
 
 *Git Ref*
@@ -31,22 +34,18 @@ _98_
 _andryluthfi_
 
 *Job Status*
-_SUCCESS_
-`;
-    const data = JSON.stringify({
-        "chatId": numbers,
-        "body": body
+_SUCCESS_`
     });
 
     const config = {
         method: 'post',
-        url: 'https://dulido-bot.herokuapp.com/api/1000/sendMessage',
+        url: url,
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Cookie': 'connect.sid=s%3ARPpaOmzVCl4j8kY9dGRutM-o4-_aRShw.%2FhyCSqB3aC9vr3o3e3%2FlYrPCO5LA9qp0ISLXCRZDea0'
         },
         data: data
     };
-
     axios(config)
         .then(function (response) {
             console.log(JSON.stringify(response.data));
@@ -55,8 +54,6 @@ _SUCCESS_
             console.log(error);
         });
 }
-
-
 module.exports = {
     report
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const core = require("@actions/core");
 const { report } = require('./httpClient');
 const { GithubActions } = require('./githubActions');
 
-const url = core.getInput('url')
+const url =  core.getInput('url')
 const numbers = core.getInput('numbers')
 const data = core.getInput('data')
 
-report({ url, numbers, data: new GithubActions() })
+report({ url, numbers, data, actions: new GithubActions() })
